412fcbc685d07876d455246dab8d65d3e9d24f66
olipadavan <91786314+olipadavan@users.noreply.github.com>
2023-09-25 12:53:02 +0200

    bugfix

--- src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
+++ src/main/kotlin/hu/obuda/devops/fibonaccirestapi/service/FibonacciService.kt
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service
 @Service
 class FibonacciService {
     fun fibonacci(n: Int): Int {
-        return if (n == 1) 1
+        return if (n < 2) 1
         else fibonacci(n-1) + fibonacci(n-2)
         // TODO instead of this logic implement fibonacci
     }
